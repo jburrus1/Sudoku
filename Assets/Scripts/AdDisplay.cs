2598c33ef8f13614c79409d2756dd227dd4a1f14
Jack Burrus <jackwburrus@gmail.com>
2024-01-30 11:01:40 -0600

    Fix for banner ads

--- Assets/Scripts/AdDisplay.cs
+++ Assets/Scripts/AdDisplay.cs
@@ -2,23 +2,27 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Advertisements;
-public class AdDisplay : MonoBehaviour
+public class AdDisplay : MonoBehaviour, IUnityAdsInitializationListener
 {
-    public string myGameIdAndroid = "5541405";
-    public string myGameIdIOS = "5541404";
-    public string adUnitIdAndroid = "Banner_Android";
-    public string adUnitIdIOS = "Banner_iOS";
-    public string myAdUnitId; 
+    private string myGameIdAndroid = "5541405";
+    private string myGameIdIOS = "5541404";
+    private string adUnitIdAndroid;
+    private string adUnitIdIOS;
+    public string myAdUnitId;
     public bool adStarted;
+    public bool adLoading;
     // Start is called before the first frame update
     void Start()
     {
+        adUnitIdAndroid = "Banner_Android";
+        adUnitIdIOS = "Banner_iOS";
+        adStarted = false;
         Advertisement.Banner.SetPosition(BannerPosition.BOTTOM_CENTER);
 #if UNITY_IOS
 	        Advertisement.Initialize(myGameIdIOS, false);
 	        myAdUnitId = adUnitIdIOS;
 #else
-        Advertisement.Initialize(myGameIdAndroid, false);
+        Advertisement.Initialize(myGameIdAndroid, false,this);
         myAdUnitId = adUnitIdAndroid;
 #endif
     }
@@ -26,23 +30,29 @@ public class AdDisplay : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
-        if (Advertisement.isInitialized && !adStarted)
-        {
-            Advertisement.Banner.Load(myAdUnitId);
-            Advertisement.Banner.Show(myAdUnitId);
-            adStarted = true;
-        }
+    }
 
-        if (Advertisement.Banner.isLoaded)
-        {
-            
-            GetComponent<UnityEngine.UI.Image>().enabled = false;
-        }
-        else
-        {
-            
-            GetComponent<UnityEngine.UI.Image>().enabled = true;
-        }
+    public void OnInitializationComplete()
+    {
+        Debug.Log("Ads initialized, now loading");
+        var bannerOptions = new BannerLoadOptions();
+        bannerOptions.loadCallback = LoadCallback;
+        bannerOptions.errorCallback = LoadErrorCallback;
+        Advertisement.Banner.Load(myAdUnitId, bannerOptions);
+    }
+
+    public void LoadCallback()
+    {
+        Debug.Log("Ad loaded, now showing");
+        Advertisement.Banner.Show(myAdUnitId);
+    }
+    public void LoadErrorCallback(string error)
+    {
+        Debug.Log("Load Failed with error: " + error);
+    }
+
+    public void OnInitializationFailed(UnityAdsInitializationError error, string message)
+    {
+        throw new System.NotImplementedException();
     }
 }
